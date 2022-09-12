eec5a0e58fefff4fdaea6c38a302cadd261822a2
tabassum2507 <10tabassum2@gmail.com>
2022-09-12 19:21:55 +0530

    chat started

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -2,7 +2,27 @@ import React from 'react'
 
 const Input = () => {
   return (
-    <div>Input</div>
+    <div className="input">
+      <input
+        type="text"
+        placeholder="Type something..."
+        // onChange={(e) => setText(e.target.value)}
+        // value={text}
+      />
+      <div className="send">
+        <img src="https://images.unsplash.com/photo-1655874819398-c6dfbec68ac7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGF2YXRhcnxlbnwwfDJ8MHx8&auto=format&fit=crop&w=500&q=60" alt="" />
+        <input
+          type="file"
+          style={{ display: "none" }}
+          id="file"
+          // onChange={(e) => setImg(e.target.files[0])}
+        />
+        <label htmlFor="file">
+          <img src="https://images.unsplash.com/photo-1655874819398-c6dfbec68ac7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGF2YXRhcnxlbnwwfDJ8MHx8&auto=format&fit=crop&w=500&q=60" alt="" />
+        </label>
+        <button >Send</button>
+      </div>
+    </div>
   )
 }
 
--- src/components/Meassage.jsx
+++ src/components/Meassage.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 const Meassage = () => {
   return (
-    <div>Meassage</div>
+    <div
+    
+      className="message"
+    >
+      <div className="messageInfo">
+        <img
+          src="https://images.unsplash.com/photo-1655874819398-c6dfbec68ac7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGF2YXRhcnxlbnwwfDJ8MHx8&auto=format&fit=crop&w=500&q=60"
+          alt=""
+        />
+        <span>just now</span>
+      </div>
+      <div className="messageContent">
+        <p>hi</p>
+        
+      </div>
+    </div>
   )
 }
 
